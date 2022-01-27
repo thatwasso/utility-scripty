f1f1616b76ec865dba427a4553b47d8f821ee229
thatwasso <46354493+thatwasso@users.noreply.github.com>
2022-01-27 17:55:41 +0000

    Update imageoptim.sh

--- imageoptim.sh
+++ imageoptim.sh
@@ -7,16 +7,16 @@ if [ "$choice" = 'png' ]
 then
   echo "You selected PNG";
   sleep 3;
-  mogrify -resize 40% /home/debuser/Immagini/*.png;
-  exiftool -overwrite_original -all=/home/debuser/Immagini/*;
+  mogrify -resize 40% /PATH/TO/FOLDER/*.png;
+  exiftool -overwrite_original -all=/PATH/TO/FOLDER/*;
 
 elif [ "$choice" = 'jpg'  ]
 then
 
   echo "You selected JPG";
   sleep 3;
-  mogrify -resize 40% /home/debuser/Immagini/*.png;
-  exiftool -overwrite_original -all=/home/debuser/Immagini/*;
+  mogrify -resize 40% /PATH/TO/FOLDER/*.png;
+  exiftool -overwrite_original -all=/PATH/TO/FOLDER/*;
 else
 echo "invalid answer, type jpg/png";
 fi
