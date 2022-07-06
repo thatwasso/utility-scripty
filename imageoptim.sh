520087762e8db6c9dc5f8b1b26020da9387f0f6d
thatwasso <46354493+thatwasso@users.noreply.github.com>
2022-07-06 11:48:09 +0000

    imageoptim.sh
    Removed unnecessary steps since the utility of the script is very basic

--- imageoptim.sh
+++ imageoptim.sh
@@ -1,22 +1,5 @@
 #! /bin/bash
-read -p "Extension (jpg/png)" reply
-
-choice=$(echo $reply|sed 's/(.*)/L1/')
-
-if [ "$choice" = 'png' ]
-then
-  echo "You selected PNG";
-  sleep 3;
-  mogrify -resize 40% /PATH/TO/FOLDER/*.png;
-  exiftool -overwrite_original -all=/PATH/TO/FOLDER/*;
-
-elif [ "$choice" = 'jpg'  ]
-then
-
-  echo "You selected JPG";
-  sleep 3;
-  mogrify -resize 40% /PATH/TO/FOLDER/*.png;
-  exiftool -overwrite_original -all=/PATH/TO/FOLDER/*;
-else
-echo "invalid answer, type jpg/png";
-fi
+mogrify -format jpg /home/debuser/Immagini/*.png;
+rm /home/debuser/Immagini/*.png;
+mogrify -resize 40% /home/debuser/Immagini/*.jpg;
+exiftool -overwrite_original -all=/home/debuser/Immagini/*;
